3116f3ff542699d57edac452157b90f32d88108c
Emma Konrad <emma.konrad92@gmail.com>
2024-10-09 15:54:39 -0400

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 
+import Dictionary from "./Dictionary";
+
 function App() {
   return (
-    <div className="App container">
-      <h1 className="mt-5">Dictionary App</h1>
+    <div className="App">
+      <div className="container">
+        <h1 className="mt-5">Dictionary App</h1>
+        <Dictionary />
+        <footer>
+          Coded by Emma Konrad, open-sourced on Github and hosted on Netflify
+        </footer>
+      </div>
     </div>
   );
 }
